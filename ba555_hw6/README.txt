36700d70f67794c2fe96be248d56e092082d9de6
BrianAmin <tuf49787@temple.edu>
2019-05-22 21:25:25 -0400

    Finished part 2 of assignment, finished documentation and write up

---
+++ ba555_hw6/README.txt
@@ -0,0 +1,38 @@
+CS570 Assignment 6
+ba555
+Bhautik Amin
+Drexel ID: 13618816
+
+
+My submission contains the following files:
+    - ba555_hw6p1.java
+    - ba555hw6p2.java
+    - MergeSort.java
+    - QuickSort.java
+    - InsertionSort.java
+    - SelectionSort.java
+
+
+Part 1:
+
+
+
+
+
+Part 2:
+
+This program revovles around a main program loop. Upon execution the program first prompts the user for input. This is performed using a series of try-catch statements and if-else statements for user input validation. 
+
+The first level of user input validation is the try-catch statement, which aims to see if the user input matches the data type the Java scanner object is expecting. In any case where the data type does not match, the program prints that the input is wrong (as well as the Java exception) and then quits the program. The user will then have to rerun the program. 
+
+The second level of input validation is boundary validation. In which the given input is checked if it meets requirements specified in the assignment:
+    - Sales Person Number: [1,5] (Between 1 and 5)
+    - Product Number: [1,4] (Between 1 and 4)
+    - Amount: Must be greater than or equal to 0 (No Negative numbers)
+If any of the given data does not meet the validation, the program will print that it is invalid and will then quit. The user will have to rerun the program
+
+At the end of these three inputs, the program tabulates the data then it will ask if the user wants to continue inputting data. This is given by a input for either Y/N or y/n. Where Y/y is to continue data input, and N/n is to move onto tabuling data and calculations/printing. If the input given here is not either Y/y or N/n the program moves onto calculations and printing
+
+For printing, I used the printf functions to print out formatted headers for the table, as well as used the function to format the tabulated and computed values. I used a set of nested for loops to navigate the rows and columns of my 2D array. 
+
+After the program prints out the table, it will automatically exit
--- ba555_hw6/ba555_hw6p2.java
+++ ba555_hw6/ba555_hw6p2.java
@@ -19,17 +19,20 @@ import java.util.InputMismatchException;
     This input is checked for possible type errors (Mismatch exceptions) and then checked if it 
     meets the boundaries specfied in the assignment outlines (user validation)
 
-    The data saved to a 2-D array and totals are calculated by rows and by columns
+    If mismatch errors occur, the program quits and the user must run the program again
 
-    The program that outputs the formatted table to console and quits the program
+    The data is saved to a 2-D array and totals are calculated by navigating through the rows and by columns
+
+    The program then outputs the formatted table to the console and quits the program
 
 */
 
 
 public class ba555_hw6p2 {
 
+
     public static void main(String[] args) {
-        // Declare resuable variables for sales people and the product #
+        // Declare resuable variables for sales people and the product number
         int sales_worker = 0;
         int product = 0;
         // Declare variable for amount
@@ -57,6 +60,18 @@ public class ba555_hw6p2 {
         {   // Prompt user for data
             while(user_input)
             {
+
+                /*
+                    The following try-catch blocks are for the user input prompting
+                    The exception that might be caught would be if the user inputs a string rather than a integer/double and vice versa
+                    Along with this exception catching, user validation takes place to ensure the input meets the dictated boundaries:
+                    Sales Person Number: [1,5]
+                    Product Number: [1,4]
+                    Amount: Must be greater than or equal to 0 (No Negative numbers)
+                    Continue: Y/N or y/n to continue with user prompting or to move onto tabulating and calculations
+
+                    If a mismatch error occurs here the program will quit and the user will have to restart it
+                */
 
                 // Try-Catch block for sales worker number
                 try
@@ -146,6 +161,7 @@ public class ba555_hw6p2 {
                 }
 
                 // Update table since we got past all user validation
+                // Index by 0 so we must subtract one for row and column
                 sales_table[sales_worker-1][product-1] += amount;
                 
 
@@ -183,39 +199,55 @@ public class ba555_hw6p2 {
                 
 
             }
+
+
             // Display table
+
+            // Print the headers. Using printf to format white spacing between the values these values were selected from repetitive testing
             System.out.printf("%7s%14s%14s%14s%14s%9s\n", "Product", "Salesperson 1","Salesperson 2", "Salesperson 3", "Salesperson 4","Total");
-            // For loop to cross rows
+            // For loop to navigate rows of array
             for (int r=0; r <= 3; r++)
             {
+                // Declare empty variable for cross totals (totals along rows)
                 double cross_total = 0.0;
-                // Print the product number
-                System.out.printf("%d", (r+1));
-                // For loop to cross the columns
+                // Print the product numbers column for every row
+                // %d for integer
+                System.out.printf("%d", (r + 1 ) );
+
+                // For loop to navigate the columns of the array
                 for (int c = 0; c < 4; c++)
                 {
+                    // Print corresponding column and row value from the table
                     System.out.printf("%14.2f", sales_table[c][r]);
+                    // Update the row's cross total
                     cross_total += sales_table[c][r];
                 }
+                // Print the row's total and then move onto the next row by printing new line
                 System.out.printf("%14.2f\n", cross_total);
             }
-            // Compute grand totals at bottom of the table
-            System.out.printf("%5s\n", "Grand");
+
+
+            // Compute grand totals at bottom of the table (column totals)
+            System.out.printf("%5s\n", "Sales");
+            System.out.printf("%5s\n", "Person");
             System.out.printf("%5s", "Total");
-            // Will need another for loop to navigate over the columns and rows
+            // Will need another for loop to navigate over the columns and rows of the array
             for (int c = 0; c < 4; c++)
             {
                 for (int r = 0; r <= 3; r++)
                 {
+                    // Moving along each column, and each row, compute grand total for each individual employee
                     grand_total += sales_table[c][r];
 
                 }
+                // Print out the grand total for the individual employee
                 System.out.printf("%13.2f", grand_total);
                 // Reset variable for next column computation
                 grand_total = 0.0;
 
             }
             // Quit Program
+            System.out.println();
             System.out.println("Thank you, exiting the program");
             main_loop = false;
         }
