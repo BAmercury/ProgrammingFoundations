00e8f98c02884035d362260e2750f25ee487d340
BrianAmin <tuf49787@temple.edu>
2019-05-29 17:31:03 -0400

    Cleaning the repo real quick

--- ba555_hw7/ThreeDShape.java
+++ ba555_hw7/ThreeDShape.java
@@ -9,11 +9,6 @@ public abstract class ThreeDShape extends Shape
         super(shape_name);
     }
 
-    // Get Area and Volume Methods
-    // This method will be different for each shape so make it abstract
-    // Indvidual shape subclasses will inherit this method and change it to fit the needs
-    //public abstract double get_area();
-    //public abstract double get_volume();
 
 }
 
